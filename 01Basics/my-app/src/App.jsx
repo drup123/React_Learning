5d08177fbae0b5944b8a9f4c03c589d9711ea476
Drup Patil <145984896+drup123@users.noreply.github.com>
2025-06-05 00:20:36 +0530

    Add files via upload

---
+++ 01Basics/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Trial from "./Trial"
+
+function App() {
+ 
+
+  return (
+    <>                                   
+    <h1>Okay let's start</h1>
+    <Trial/>
+    </>
+  )
+}
+
+export default App
---
+++ 01Basics/my-app/src/Trial.jsx
@@ -0,0 +1,7 @@
+function Trial() {
+    return (
+        <h3>Hello I am Captain America</h3>
+    )
+}
+
+export default Trial;
